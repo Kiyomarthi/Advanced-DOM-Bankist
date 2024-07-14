b8303fb6b64f7e3d58d1a8432cfb1f9b27dc2400
Kiyomarthi <a.kiyomarthi@gmail.com>
2024-07-14 15:37:56 +0330

    feat: sticky nav when scroll page

--- script.js
+++ script.js
@@ -205,3 +205,13 @@ dotContainer.addEventListener('click', function (e) {
     dotActive(slide);
   }
 });
+
+const initialCoords = section1.getBoundingClientRect();
+
+window.addEventListener('scroll', function () {
+  if (window.scrollY > initialCoords.top) {
+    nav.classList.add('sticky');
+  } else {
+    nav.classList.remove('sticky');
+  }
+});
